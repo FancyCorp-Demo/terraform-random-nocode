f954b7fdac7f93ea0b05a9f502e5fd4faf69afa9
Lucy Davinhart <lucy@lucymhdavies.com>
2026-05-28 09:42:58 +0100

    Remove default values from all vars
    As a no-code module, HCP TF will require the user to specify these
    values

--- vars.tf
+++ vars.tf
@@ -4,11 +4,21 @@ variable "prefix" {
 }
 
 variable "separator" {
-  type    = string
-  default = "-"
+  type = string
+  validation {
+    condition     = contains(["-", "_", "/"], var.separator)
+    error_message = "Separator must be one of: - _ /"
+  }
 }
 
 variable "length" {
-  type    = number
-  default = 2
+  type = number
+  validation {
+    condition     = var.length >= 1
+    error_message = "Length must be a positive"
+  }
+  validation {
+    condition     = var.length == floor(var.length)
+    error_message = "Length must be an integer"
+  }
 }
